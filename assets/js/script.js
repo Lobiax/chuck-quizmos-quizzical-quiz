f3bfdb20211737a8cd4363c7fec56332b9bb30c9
Lobiax <>
2023-07-24 17:17:02 +0000

    Add startGame function and displayquestion idea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
-function startGame();
-function checkAnswer();
-function incrementScore(){
-    let previousScore = parseInt(document.getElementById("score").innerHTML)
-    document.getElementById("score").innerHTML = ++previousScore; 
+document.addEventListener("DOMContentLoaded", startGame());
+
+document.getElementById("answer1")
+function startGame() {
+    displayQuestion1();
+    console.log("The game has started");
 }
-function incrementIncorrectAnswer(){
-    let previousIncorrect = parseInt(document.getElementById("incorrect").innerHTML)
+function checkAnswer() {
+
+}
+/** This increments the score counter */
+function incrementScore() {
+    let previousScore = parseInt(document.getElementById("score").innerHTML);
+    document.getElementById("score").innerHTML = ++previousScore;
+}
+/**This increments the incorrect answers counter */
+function incrementIncorrectAnswer() {
+    let previousIncorrect = parseInt(document.getElementById("incorrect").innerHTML);
     document.getElementById("incorrect").innerHTML = ++previousIncorrect;
 }
-function displayQuestion1();
-function displayQuestion2();
-function displayQuestion3();
-function displayQuestion4();
-function displayQuestion5();
+
+/** */
+function displayQuestion1() {
+    document.getElementById("question").textContent = "What Pharaoh does the Great Sphynx of Giza represent?";
+    document.getElementById("answer1").textContent = "Tut";
+    document.getElementById("answer2").textContent = "Khafre";
+    document.getElementById("answer3").textContent = "Khufu";
+    document.getElementById("answer4").textContent = "Cleopatra";
+}
+
