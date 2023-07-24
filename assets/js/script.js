adb7cbbe7fc73331c1ed77cb0a86a68a51273934
Lobiax <>
2023-07-24 15:22:43 +0000

    Added javascript for incrementing score/incorrect answ

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
+function startGame();
 function checkAnswer();
-function incrementScore();
-function incrementIncorrectAnswer();
+function incrementScore(){
+    let previousScore = parseInt(document.getElementById("score").innerHTML)
+    document.getElementById("score").innerHTML = ++previousScore; 
+}
+function incrementIncorrectAnswer(){
+    let previousIncorrect = parseInt(document.getElementById("incorrect").innerHTML)
+    document.getElementById("incorrect").innerHTML = ++previousIncorrect;
+}
 function displayQuestion1();
 function displayQuestion2();
 function displayQuestion3();
