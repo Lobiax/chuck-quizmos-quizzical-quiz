42fa70c94b6384f088ee8c256a48bfb43ee3042d
Lobiax <>
2023-07-26 12:47:28 +0000

    made event listener for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,73 +2,75 @@ let quizQuestions = [
     {
         question: "What Pharaoh does the Great Sphynx of Giza represent?",
         answers: [
-            { answer1: "Tut", answer: false },
-            { answer2: "Khafre", answer: true },
-            { answer3: "Khufu", answer: false },
-            { answer4: "Cleopatra", answer: false }
+            { option: "Tut", answer: false },
+            { option: "Khafre", answer: true },
+            { option: "Khufu", answer: false },
+            { option: "Cleopatra", answer: false }
         ],
     },
     {
         question: "Which country consumes the most chocolate per capita?",
         answers: [
-            { answer1: "Switzerland", answer: true },
-            { answer2: "Sweden", answer: false },
-            { answer3: "USA", answer: false },
-            { answer4: "Brazil", answer: false }
+            { option: "Switzerland", answer: true },
+            { option: "Sweden", answer: false },
+            { option: "USA", answer: false },
+            { option: "Brazil", answer: false }
         ],
     },
     {
         question: "Directing a small green amphibian across a busy road was the goal in which classic video game?",
         answers: [
-            { answer1: "Toads", answer: false },
-            { answer2: "Salamander", answer: false },
-            { answer3: "Battletoads", answer: false },
-            { answer4: "Frogger", answer: true }
+            { option: "Toads", answer: false },
+            { option: "Salamander", answer: false },
+            { option: "Battletoads", answer: false },
+            { option: "Frogger", answer: true }
         ],
     },
     {
         question: "What mystical blade features in virtually every Legend of Zelda game?",
         answers: [
-            { answer1: "The Sword of Legends", answer: false },
-            { answer2: "Sword of Light", answer: false },
-            { answer3: "Claymore", answer: false },
-            { answer4: "The Master Sword", answer: true }
+            { option: "The Sword of Legends", answer: false },
+            { option: "Sword of Light", answer: false },
+            { option: "Claymore", answer: false },
+            { option: "The Master Sword", answer: true }
         ],
     },
     {
         question: "What country has more than half of its people believing in elves, dwarves and ghosts?",
         answers: [
-            { answer1: "Romania", answer: false },
-            { answer2: "Iceland", answer: true },
-            { answer3: "China", answer: false },
-            { answer4: "Sudan", answer: false }
+            { option: "Romania", answer: false },
+            { option: "Iceland", answer: true },
+            { option: "China", answer: false },
+            { option: "Sudan", answer: false }
         ],
     },
     {
-        question: "What shoes were originally created for men but are more often worn by women?",
+        question: "What shoes were originally created for men but are now more often worn by women?",
         answers: [
-            { answer1: "Ballerina Flats", answer: false },
-            { answer2: "Sandalettes", answer: false },
-            { answer3: "High Heels", answer: true },
-            { answer4: "Moccasins", answer: false }
+            { option: "Ballerina Flats", answer: false },
+            { option: "Sandalettes", answer: false },
+            { option: "High Heels", answer: true },
+            { option: "Moccasins", answer: false }
         ],
     },
     {
         question: "What country has a unicorn part of its national emblem?",
         answers: [
-            { answer1: "Scotland", answer: true },
-            { answer2: "Finland", answer: false },
-            { answer3: "France", answer: false },
-            { answer4: "Iran", answer: false }
+            { option: "Scotland", answer: true },
+            { option: "Finland", answer: false },
+            { option: "France", answer: false },
+            { option: "Iran", answer: false }
         ],
     },
 ];
 
-let answerButtons = document.querySelectorAll("button");
 let questionText = document.getElementById("question");
+let answerButton = document.getElementsByClassName("answer");
+let answerArray = Array.from(answerButton);
 
 document.addEventListener("DOMContentLoaded", startGame());
 
+/** This functions starts the game */
 function startGame() {
     displayQuestion();
     console.log("The game has started");
@@ -76,13 +78,28 @@ function startGame() {
 
 /** This function displays the questions */
 function displayQuestion() {
-    let questionText = document.getElementById("question");
     questionText.innerHTML = quizQuestions[0].question;
-
+    for (let i = 0; i < answerButton.length; i++) {
+        let btn = answerButton[i];
+        btn.innerHTML = quizQuestions[0].answers[i].option;
+    }
 }
+answerArray.forEach(element => {
+    element.addEventListener("click", checkAnswer);
+});
+
+/** This function checks whether the answer is correct or incorrect */
 function checkAnswer() {
+    if (quizQuestions.option == true) {
+        incrementScore();
+        console.log("true");
+    } else {
+        incrementIncorrectAnswer();
+        console.log("false");
+    }
 
 }
+
 /** This increments the score counter */
 function incrementScore() {
     let previousScore = parseInt(document.getElementById("score").innerHTML);
