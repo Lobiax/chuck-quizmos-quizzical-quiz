07152b3a0b8680e080c0ac4c2289a994cec3b01e
Lobiax <>
2023-07-26 09:13:41 +0000

    Put questions in dictionary and changed dispquest func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,84 @@
+let quizQuestions = [
+    {
+        question: "What Pharaoh does the Great Sphynx of Giza represent?",
+        answers: [
+            { answer1: "Tut", answer: false },
+            { answer2: "Khafre", answer: true },
+            { answer3: "Khufu", answer: false },
+            { answer4: "Cleopatra", answer: false }
+        ],
+    },
+    {
+        question: "Which country consumes the most chocolate per capita?",
+        answers: [
+            { answer1: "Switzerland", answer: true },
+            { answer2: "Sweden", answer: false },
+            { answer3: "USA", answer: false },
+            { answer4: "Brazil", answer: false }
+        ],
+    },
+    {
+        question: "Directing a small green amphibian across a busy road was the goal in which classic video game?",
+        answers: [
+            { answer1: "Toads", answer: false },
+            { answer2: "Salamander", answer: false },
+            { answer3: "Battletoads", answer: false },
+            { answer4: "Frogger", answer: true }
+        ],
+    },
+    {
+        question: "What mystical blade features in virtually every Legend of Zelda game?",
+        answers: [
+            { answer1: "The Sword of Legends", answer: false },
+            { answer2: "Sword of Light", answer: false },
+            { answer3: "Claymore", answer: false },
+            { answer4: "The Master Sword", answer: true }
+        ],
+    },
+    {
+        question: "What country has more than half of its people believing in elves, dwarves and ghosts?",
+        answers: [
+            { answer1: "Romania", answer: false },
+            { answer2: "Iceland", answer: true },
+            { answer3: "China", answer: false },
+            { answer4: "Sudan", answer: false }
+        ],
+    },
+    {
+        question: "What shoes were originally created for men but are more often worn by women?",
+        answers: [
+            { answer1: "Ballerina Flats", answer: false },
+            { answer2: "Sandalettes", answer: false },
+            { answer3: "High Heels", answer: true },
+            { answer4: "Moccasins", answer: false }
+        ],
+    },
+    {
+        question: "What country has a unicorn part of its national emblem?",
+        answers: [
+            { answer1: "Scotland", answer: true },
+            { answer2: "Finland", answer: false },
+            { answer3: "France", answer: false },
+            { answer4: "Iran", answer: false }
+        ],
+    },
+];
+
+let answerButtons = document.querySelectorAll("button");
+let questionText = document.getElementById("question");
+
 document.addEventListener("DOMContentLoaded", startGame());
 
-document.getElementById("answer1");
 function startGame() {
-    displayQuestion1();
+    displayQuestion();
     console.log("The game has started");
+}
+
+/** This function displays the questions */
+function displayQuestion() {
+    let questionText = document.getElementById("question");
+    questionText.innerHTML = quizQuestions[0].question;
+
 }
 function checkAnswer() {
 
@@ -17,57 +92,5 @@ function incrementScore() {
 function incrementIncorrectAnswer() {
     let previousIncorrect = parseInt(document.getElementById("incorrect").innerHTML);
     document.getElementById("incorrect").innerHTML = ++previousIncorrect;
-}
-
-/** This function displays the first question */
-function displayQuestion1() {
-    document.getElementById("question").textContent = "What Pharaoh does the Great Sphynx of Giza represent?";
-    document.getElementById("answer1").textContent = "Tut";
-    document.getElementById("answer2").textContent = "Khafre";
-    document.getElementById("answer3").textContent = "Khufu";
-    document.getElementById("answer4").textContent = "Cleopatra";
-}
-
-function displayQuestion2() {
-    document.getElementById("question").textContent = "Which country consumes the most chocolate per capita?";
-    document.getElementById("answer1").textContent = "Switzerland";
-    document.getElementById("answer2").textContent = "Sweden";
-    document.getElementById("answer3").textContent = "USA";
-    document.getElementById("answer4").textContent = "Brazil";
-}
-function displayQuestion3() {
-    document.getElementById("question").textContent = "Directing a small green amphibian across a busy road was the goal in which classic video game?";
-    document.getElementById("answer1").textContent = "Toads";
-    document.getElementById("answer2").textContent = "Salamander";
-    document.getElementById("answer3").textContent = "Battletoads";
-    document.getElementById("answer4").textContent = "Frogger";
-}
-function displayQuestion4() {
-    document.getElementById("question").textContent = "What mystical blade features in virtually every Legend of Zelda game?";
-    document.getElementById("answer1").textContent = "The Sword of Legends";
-    document.getElementById("answer2").textContent = "Sword of Light";
-    document.getElementById("answer3").textContent = "Claymore";
-    document.getElementById("answer4").textContent = "The Master Sword";
-}
-function displayQuestion5() {
-    document.getElementById("question").textContent = "What country has more than half of its people believing in elves, dwarves and ghosts?";
-    document.getElementById("answer1").textContent = "Romania";
-    document.getElementById("answer2").textContent = "Iceland";
-    document.getElementById("answer3").textContent = "China";
-    document.getElementById("answer4").textContent = "Sudan";
-}
-function displayQuestion6() {
-    document.getElementById("question").textContent = "What shoes were originally created for men but are more often worn by women? ";
-    document.getElementById("answer1").textContent = "Ballerina Flats";
-    document.getElementById("answer2").textContent = "Sandalettes";
-    document.getElementById("answer3").textContent = "High Heels";
-    document.getElementById("answer4").textContent = "Moccasins";
-}
-function displayQuestion7() {
-    document.getElementById("question").textContent = "What country has a unicorn part of its national emblem? ";
-    document.getElementById("answer1").textContent = "Scotland";
-    document.getElementById("answer2").textContent = "Finland";
-    document.getElementById("answer3").textContent = "France";
-    document.getElementById("answer4").textContent = "Sudan";
 }
 
